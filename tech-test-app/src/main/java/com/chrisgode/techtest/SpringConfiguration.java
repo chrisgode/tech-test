4614157a1a0decd3d5c8499b79e1e6db735bab16
Christian <christian.godefroy@axxiom.com.br>
2016-04-19 14:34:48 -0300

    Make the user acceptance test pass

--- tech-test-app/src/main/java/com/chrisgode/techtest/SpringConfiguration.java
+++ tech-test-app/src/main/java/com/chrisgode/techtest/SpringConfiguration.java
@@ -1,19 +1,15 @@
-package com.chrisgode.techtest.tests;
-
-import com.chrisgode.techtest.SpringConfiguration;
+package com.chrisgode.techtest;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 @Configuration
-@Import({SpringConfiguration.class})
 @ComponentScan
-@PropertySource("classpath:tests.properties")
-public class AcceptanceTestsConfiguration {
+@PropertySource("classpath:application.properties")
+public class SpringConfiguration {
 	
 	@Bean
 	public static PropertySourcesPlaceholderConfigurer propertyConfigInDev() {
